fd99411a11ee2d267752a4b627a2e31730ce0232
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-09-10 03:15:00 +0000

    Sep 10, 2024, 10:15 AM

--- src/main/java/com/creditfool/warung_makan_bahari/repository/CustomerRepository.java
+++ src/main/java/com/creditfool/warung_makan_bahari/repository/CustomerRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.creditfool.warung_makan_bahari.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-
 }
