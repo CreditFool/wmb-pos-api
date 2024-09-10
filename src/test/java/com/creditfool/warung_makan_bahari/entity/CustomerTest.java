766e76b3e4a25ed1397ba828efdb0eb961f050bf
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-09-10 03:09:08 +0000

    Sep 10, 2024, 10:09 AM

--- src/test/java/com/creditfool/warung_makan_bahari/entity/CustomerTest.java
+++ src/test/java/com/creditfool/warung_makan_bahari/entity/CustomerTest.java
@@ -73,12 +73,4 @@ class CustomerTest {
         assertEquals(updatedAt, customer.getUpdatedAt());
         assertEquals(deletedAt, customer.getDeletedAt());
     }
-
-    // Creating a Customer object with null values for non-nullable fields
-    @Test
-    void test_create_customer_with_null_values_for_non_nullable_fields() {
-        assertThrows(NullPointerException.class, () -> {
-            new Customer(null, null, null, null, null, null, null);
-        });
-    }
 }
